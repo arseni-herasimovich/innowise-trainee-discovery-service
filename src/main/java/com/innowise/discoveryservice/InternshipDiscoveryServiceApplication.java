588e69491611985dc4df37fc9123ba1bf42dae1b
arseni-herasimovich <arseni.herasimovich@innowise.com>
2025-11-03 19:05:07 +0300

    git init

---
+++ src/main/java/com/innowise/discoveryservice/InternshipDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipDiscoveryServiceApplication.class, args);
+    }
+
+}
